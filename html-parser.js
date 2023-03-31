567f1d87771081b1c92e3895cf446f4c36f06de5
mike <mgalakos@gmail.com>
2023-03-31 14:08:55 +0300

    tests

--- html-parser.js
+++ html-parser.js
@@ -2,29 +2,32 @@ import * as parse5 from 'parse5'
 import { adapter } from 'parse5-htmlparser2-tree-adapter'
 
 /**
+ * @typedef {import("domhandler").Document} Document
  * @typedef {import("domhandler").Element} Element
- */
+ * @typedef {import("domhandler").Text} Text
+ * @typedef { Element | Text } Node
+*/
 
 
-/** @param {string} content */
-export function parseHtml(content) {
-  return parse5.parse(content, { treeAdapter: adapter })
+/** @param {string} html */
+export function parseHtml(html) {
+  return parse5.parse(html, { treeAdapter: adapter })
 }
 
 
-/** @param {string} content */
-export function parseFragment(content) {
-  return parse5.parseFragment(content, { treeAdapter: adapter })
+/** @param {string} html */
+export function parseFragment(html, fragmentContext = createElement('div')) {
+  return parse5.parseFragment(fragmentContext, html, { treeAdapter: adapter })
 }
 
 
-/** @param {Element} node */
+/** @param {Element | Document} node */
 export function serialize(node) {
   return parse5.serialize(node, { treeAdapter: adapter })
 }
 
 
-/** @param {Element} node */
+/** @param {Element | Document} node */
 export function serializeOuter(node) {
   return parse5.serializeOuter(node, { treeAdapter: adapter })
 }
@@ -37,42 +40,34 @@ export function createElement(tagName, attributes = {}) {
 }
 
 
-/** @param {Element} document */
-export function documentBody(document) {
-  return qs(document, el => el.name === 'body')
-}
-
-
-/** @type {(node: Element, predicate: (node: Element) => boolean) => Element | undefined} */
+/** @type {(node: Element | Document, predicate: (node: Node) => boolean) => Node | undefined} */
 export function qs(node, predicate) {
   let result
-  if (predicate(node)) {
+  if (node.type !== 'root' && predicate(node)) {
     result = node
   } else {
-    if (node.children) {
-      for (let child of node.children) {
-        if (adapter.isElementNode(child)) {
-          result = qs(child, predicate)
-          if (result) break
-        }
-      }
+    for (let child of node.children) {
+      if (adapter.isElementNode(child)) result = qs(child, predicate)
+      if (child.type === 'text' && predicate(child)) result = child
+      if (result) break
     }
   }
   return result
 }
 
 
-/** @type {(node: Element, predicate: (node: Element) => boolean, res?: Element[]) => Element[]} */
+/** @type {(node: Element | Document, predicate: (node: Node) => boolean, res?: Node[]) => Node[]} */
 export function qsa(node, predicate, res = []) {
-  if (predicate(node)) res.push(node)
-  if (node.children) node.children.forEach(child => {
+  if (node.type !== 'root' && predicate(node)) res.push(node)
+  for (let child of node.children) {
     if (adapter.isElementNode(child)) qsa(child, predicate, res)
-  })
+    if (child.type === 'text' && predicate(child)) res.push(child)
+  }
   return res
 }
 
 
-/** @type {(newChild: Element, refChild: Element) => void} */
+/** @type {(newChild: Node, refChild: Node) => void} */
 export function insertBefore(newChild, refChild) {
   if (!newChild || !refChild) throw new Error('missing parameter')
   if (!refChild.parent) throw new Error('insertBefore Error: refChild has no parent element')
@@ -80,7 +75,7 @@ export function insertBefore(newChild, refChild) {
 }
 
 
-/** @type {(parentNode: Element, newNode: Element) => void} */
+/** @type {(parentNode: Element, newNode: Node) => void} */
 export function appendChild(parentNode, newNode) {
   if (!parentNode || !newNode) throw new Error('missing parameter')
   adapter.appendChild(parentNode, newNode)
@@ -94,10 +89,10 @@ export function insertText(parentNode, text) {
 }
 
 
-/** @param {Element} el */
-export function remove(el) {
-  if (!el) throw new Error('missing parameter')
-  adapter.detachNode(el)
+/** @param {Node} node */
+export function detachNode(node) {
+  if (!node) throw new Error('missing parameter')
+  adapter.detachNode(node)
 }
 
 
@@ -114,28 +109,30 @@ export function innerHTML(el, html) {
   if (!el || html == null) throw new Error('missing parameter')
   if (!(typeof html === 'string')) throw new TypeError('innerHTML: html param must be a string')
   el.children = []
-  // const fragment = parseFragment(html)
-  const fragment = parse5.parseFragment(el, html, { treeAdapter: adapter })
-  if (fragment.children) {
-    for (const child of fragment.children) {
-      if (adapter.isElementNode(child)) appendChild(el, child)
-    }
+  const fragment = parseFragment(html, el)
+  for (const child of fragment.children) {
+    if (adapter.isElementNode(child)) appendChild(el, child)
   }
 }
 
 
 /** @param {Element} el */
-export function clone(el) {
+export function cloneElement(el) {
   const newEl = createElement(el.tagName)
   adapter.adoptAttributes(newEl, adapter.getAttrList(el))
   const children = adapter.getChildNodes(el)
   for (const child of children) {
-    let newChild
     if (adapter.isElementNode(child)) {
-      newChild = clone(child)
+      const newChild = cloneElement(child)
       appendChild(newEl, newChild)
     }
     if (adapter.isTextNode(child)) adapter.insertText(newEl, child.data)
   }
   return newEl
+}
+
+
+/** @param {Document} document */
+export function documentBody(document) {
+  return qs(document, el => el.type === 'tag' && el.name === 'body')
 }
--- test/10-simple-html.js
+++ test/10-simple-html.js
@@ -1,7 +1,23 @@
-import fs from 'node:fs/promises'
 import assert from 'node:assert'
 import * as html from '../html-parser.js'
-const __dirname = new URL('.', import.meta.url).pathname
+
+
+const contentHTML = /*html*/`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <div>
+      simple document
+    </div>
+  </body>
+  </html>
+`
 
 const contentFragment = /*html*/`
   <section>
@@ -11,29 +27,49 @@ const contentFragment = /*html*/`
 `
 
 
-describe('custom methods', () => {
-
-  it('insertAfter', async () => {
-    assert.fail('not implemented')
-  })
+describe('test custom methods', () => {
 
   it('innerHTML', async () => {
     const fragment = html.parseFragment(contentFragment)
-    const child1 = html.qs(fragment, o => o.attribs?.id === 'child1')
+    const child1 = html.qs(fragment, o => o.type === 'tag' && o.attribs.id === 'child1')
+    if (!child1 || child1.type !== 'tag') throw new assert.AssertionError({ message: 'error in fragment parsing' })
     html.innerHTML(child1, /*html*/`<span id="grandchild">grandchild content</span>`)
-    console.log(html.serialize(fragment))
-    assert.equal(trimLines(html.serialize(fragment)), trimLines(/*html*/`
+    assertEqualHTML(
+      html.serialize(fragment), 
+      /*html*/`
       <section>
         <div id="child1"><span id="grandchild">grandchild content</span></div>
         <div id="child2">child2 content</div>
       </section>
-    `))
+    `)
+  })
+
+  it('cloneElement', async () => {
+    const fragment = html.parseFragment(contentFragment)
+    const section = html.qs(fragment, o => o.type === 'tag' && o.name === 'section')
+    if (!section || section.type !== 'tag') throw new assert.AssertionError({ message: 'error in fragment parsing' })
+    const clonedSection = html.cloneElement(section)
+    assertEqualHTML(html.serializeOuter(section), html.serializeOuter(clonedSection))
+  })
+
+  it('documentBody', async () => {
+    const doc = html.parseHtml(contentHTML)
+    const body = html.documentBody(doc)
+    assert.notStrictEqual(body, undefined)
+    assert.strictEqual(body?.type, 'tag')
+    assert.strictEqual(body?.name, 'body')
   })
 
 })
 
 
 
+/** @param {string} content */
 function trimLines(content) {
   return content.split('\n').map(l => l.trim()).join('')
+}
+
+/** @type {(html1: string, html2: string) => void} */
+function assertEqualHTML(html1, html2) {
+  assert.strictEqual(trimLines(html1), trimLines(html2))
 }
